948b8fa72c5f76cb2df085ec6dcfab39548ab709
Deeshant Bhattacharjee <deeshantbhatta2@gmail.com>
2022-06-11 18:48:30 +0530

    Temporary commit

--- maxcoin/services/backend/MongoBackend.js
+++ maxcoin/services/backend/MongoBackend.js
@@ -1,15 +1,28 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-empty-function */
+const {MongoClient} = require('mongodb');
+
 const CoinAPI = require('../CoinAPI');
 
 class MongoBackend {
 
   constructor() {
     this.coinAPI = new CoinAPI();
+    this.mongoUrl = "mongodb://localhost:37017/maxcoin";
+    this.client = null;
+    this.collection = null;
   }
 
-  async connect() {}
+  async connect() {
+    const mongoClient = new MongoClient(this.mongoUrl, 
+      {useUnifiedTopology: true, 
+        useNewUrlParser: true});
+      
+      this.client = await mongoClient.connect();
+      this.collection = this.client.db("maxcoin").collection("values");
+      return this.client;
+  }
 
   async disconnect() {}
 
@@ -17,7 +30,19 @@ class MongoBackend {
 
   async getMax() {}
 
-  async max() {}
+  async max() {
+    console.info("Connecting to MongoDb");
+    console.time("mongodb-connect");
+    const client = await this.connect();
+
+    if (client.isConnected()) {
+      console.info("Connected to MongoDB");
+    } else {
+      throw Error("Connection to MongoDB failed");
+    }
+
+    console.timeEnd("mongodb-connect");
+  }
 }
 
 module.exports = MongoBackend;
